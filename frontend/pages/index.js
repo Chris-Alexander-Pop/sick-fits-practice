bc1adbff4a1cc65e04c31602ba5f259e84d7648d
“Chris <chrisalexanderpop@gmail.com>
2024-05-23 10:04:00 -0400

    All code up to video 05.

---
+++ frontend/pages/index.js
@@ -0,0 +1,5 @@
+export default function IndexPage() {
+  return <div>
+    <p>Hello! from IndexPage</p>
+  </div>
+}
---
+++ frontend/pages/products.js
@@ -0,0 +1,5 @@
+export default function OrderPage() {
+  return <div>
+    <p>Hey! from ProductsPage</p>
+  </div>
+}
