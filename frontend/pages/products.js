bb5d4772be63194cae2ecfa51634d8c75b1b1a84
“Chris <chrisalexanderpop@gmail.com>
2024-05-24 08:37:31 -0400

    Completed half of the project.

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,5 +1,9 @@
+import Products from '../components/Products';
+
 export default function OrderPage() {
-  return <div>
-    <p>Hey! from ProductsPage</p>
-  </div>
+  return (
+    <div>
+      <Products />
+    </div>
+  );
 }
